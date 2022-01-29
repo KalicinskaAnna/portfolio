dd7ca05f945f31062fedee8c3096025e288aa591
KalicinskaAnna <97315171+KalicinskaAnna@users.noreply.github.com>
2022-01-30 00:17:46 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+const spanColor = document.querySelector('.header__heading-hover')
+
+const addColor=()=>{
+	spanColor.classList.add('main-color')
+}
+const removeColor=()=>{
+	spanColor.classList.remove('main-color')
+}
+
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+
+	navBtnBars.classList.remove('black-bars-color')
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+	handleNacItemsAnimation()
+}
+
+const handleNacItemsAnimation = () => {
+	let delayTime = 0
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+const handleObserver = () => {
+	const currentSection = window.scrollY
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+handleCurrentYear()
+
+navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
+spanColor.addEventListener('mouseover', addColor)
+spanColor.addEventListener('mouseleave', removeColor)
+
+
+// BEM SASS REM EM MiXIN $zmienne-kolory
